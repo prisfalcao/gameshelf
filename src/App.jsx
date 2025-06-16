9decd8be4ebaf2b3d7e323f2e1f3258cfa720b0e
Priscila Falcao <priscila_falcao@dell.com>
2025-06-16 18:52:46 -0300

    Created NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AddGame from "./pages/AddGame/AddGame";
 import GameDetails from "./pages/GameDetails/GameDetails";
 import Navbar from "./components/Navbar/Navbar";
 import ImportGame from "./pages/ImportGame/ImportGame";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
@@ -11,10 +12,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/add" element={<AddGame />} />
         <Route path="/game/:id" element={<GameDetails />} />
         <Route path="/import" element={<ImportGame />} />
-        <Route path="*" element={<h1>404 - Page not found</h1>} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
