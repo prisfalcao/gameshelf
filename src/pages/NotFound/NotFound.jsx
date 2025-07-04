e45faeb4013a5a09174786118b78d4da79aae845
Priscila Falcão <falcao.priscila@hotmail.com>
2025-07-04 14:26:24 -0300

    update image

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,12 +1,12 @@
 import CustomButton from "../../components/Button/Button";
 import "./NotFound.scss";
-import logoNotFound from "../../assets/logoNotFound.svg";
+import logoEmptyState from "../../assets/logoEmptyState.svg";
 
 const NotFound = () => {
   return (
     <div className="not-found-container" role="main">
       <h1>404</h1>
-      <img src={logoNotFound} alt="GameShelf logo" />
+      <img src={logoEmptyState} alt="GameShelf logo" />
       <p>Oops! The page you're looking for doesn't exist.</p>
       <CustomButton to="/" variant="dark">
         Go back to Home
